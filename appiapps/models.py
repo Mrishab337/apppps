a359a33e9f0873c7eaea007b2b8f4cdbd41ef860
Mrishab337 <84860544+Mrishab337@users.noreply.github.com>
2024-12-03 22:41:32 +0530

    Update models.py

--- appiapps/models.py
+++ appiapps/models.py
@@ -7,4 +7,5 @@ class BusinessDetails(models.Model):
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=200)
     create_work_space_name= models.CharField(max_length=100)
-    
+    def __str__(self):
+        return self.business_name
